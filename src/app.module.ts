551c99a0863eabf4b0eb8f8de0b505d8073a6aa7
[AI11]김재영A <goodikjy@naver.com>
2024-09-06 11:25:12 +0900

    chore: prisma 기본 세팅

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
 import { PostModule } from './post/post.module';
@@ -15,18 +14,6 @@ import { ReqUserGuard } from './auth/optional-jwt.guard';
       isGlobal: true,
     }),
     AuthModule,
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env.MYSQL_HOST,
-      port: +process.env.MYSQL_PORT,
-      username: process.env.MYSQL_USER,
-      password: process.env.MYSQL_PW,
-      database: process.env.MYSQL_NAME,
-      entities: [__dirname + '/../**/*.entity.{js, ts}'],
-      synchronize: true,
-      dateStrings: false,
-      timezone: 'Z',
-    }),
     UserModule,
     PostModule,
   ],
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,12 +2,9 @@ import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { UserRepository } from './user.repository';
-import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([UserRepository]),
-  ],
+  imports: [],
   controllers: [UserController],
   providers: [UserService, UserRepository],
 })
