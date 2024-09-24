33847bfaad18d31b1922460e391bbfe331704252
[AI11]김재영A <goodikjy@naver.com>
2024-09-24 21:29:09 +0900

    🔧 Config: .env 제거 및 관련 설정
    .env를 제거하고 완전히 파라미터 스토어에만 의존하도록 변경, 파라미터 스토어 자격 증명은 aws cli를 활용

--- src/config/aws.config.ts
+++ src/config/aws.config.ts
@@ -1,25 +1,17 @@
 import { RepositoryBadGatewayException } from "@_/common/custom-error.util";
-import { GetParametersByPathCommand, SSMClient } from "@aws-sdk/client-ssm"
+import { GetParameterCommand, SSMClient } from "@aws-sdk/client-ssm"
 import { Logger } from "@nestjs/common";
 
 const loadParamsFromSsm = async (path: string) => {
-    const ssmClient = new SSMClient({ 
-        region: 'ap-northeast-2',
-        credentials: {
-            accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-            secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-        }, 
-    });
-    const command = new GetParametersByPathCommand({
-        Path: path,
-        Recursive: true,
+    const ssmClient = new SSMClient();
+    const command = new GetParameterCommand({
+        Name: path,
         WithDecryption: true,
     });
 
     try {
         const response = await ssmClient.send(command);
-        const parameters = response.Parameters;
-        const parameter = parameters.filter(x => x.Name === '/Practice/practice/dev/env')[0];
+        const parameter = response.Parameter;
         if (!parameter) {
             throw new RepositoryBadGatewayException('파라미터 스토어에서 환경변수를 못 찾았습니다.\nawsConfig 파일을 다시 한 번 확인해주세요.'); 
         };
@@ -32,6 +24,6 @@ const loadParamsFromSsm = async (path: string) => {
 }
 
 export default async () => {
-    const env = await loadParamsFromSsm('/Practice/practice/dev');
+    const env = await loadParamsFromSsm('/Practice/practice/dev/env');
     return env;
 }
